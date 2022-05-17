d8d7845e4da609f9dfa6ab75bb7fd06b28071077
Apiechota <ase21@marzyciel.pl>
2022-05-17 19:43:50 +0200

    Update run.cmd
    Projekt FamilyApp 1.1

--- run.cmd
+++ run.cmd
@@ -3,6 +3,6 @@ docker build --tag java-docker .
 docker network create family_net
 docker run --name my-h2 --network family_net -d buildo/h2database
 docker create --name rodzina --network family_net --publish 8080:8080  java-docker:latest
-cd ./Spring-familymember
+cd ../Spring-familymember
 docker build --tag java-docker2 .
 docker create --name czlonkowie --network family_net --publish 8082:8082 java-docker2:latest
